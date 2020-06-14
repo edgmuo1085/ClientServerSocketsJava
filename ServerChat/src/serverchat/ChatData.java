7363c4047f62a1817d3df47af8963d36c85e2396
edgmuo1085 <edgmuo1085@gmail.com>
2020-06-14 01:17:05 -0500

    Creación de un chat con sockets en java arquitectura cliente servidor. Tiene la opción de envio de mensajes, notificación de conexión y desconexión de clientes.

---
+++ ServerChat/src/serverchat/ChatData.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverchat;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Aula_info
+ */
+public class ChatData extends Thread {
+
+    DataOutputStream os;
+    DataInputStream is;
+    Socket client;
+    LinkedList<Socket> clients;
+    LinkedList<String> usuarios;
+    String nombre;
+
+    public ChatData(DataOutputStream os, DataInputStream is, Socket client, LinkedList<Socket> clients, LinkedList<String> usuarios, String nombre) {
+        this.os = os;
+        this.is = is;
+        this.client = client;
+        this.clients = clients;
+        this.usuarios = usuarios;
+        this.nombre = nombre;
+    }
+
+    public ChatData(Socket client, String nombre) {
+        this.client = client;
+        this.nombre = nombre;
+
+        try {
+            this.os = new DataOutputStream(client.getOutputStream());
+            this.is = new DataInputStream(client.getInputStream());
+
+        } catch (IOException ex) {
+            Logger.getLogger(ChatData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                String msj = is.readUTF();
+                System.out.println(msj);
+                for (Socket client1 : clients) {
+                    DataOutputStream os = new DataOutputStream(client1.getOutputStream());
+                    os.writeUTF(usuarios + ":" + msj);
+                }
+            } catch (IOException ex) {
+                try {
+                    this.client.close();
+                    this.os.close();
+                    this.is.close();
+                    clients.remove(this.client);
+                    usuarios.remove(this.nombre);
+                    System.out.println(usuarios + ":Todos:" + this.nombre + " se desconecto");
+
+                    for (Socket client1 : clients) {
+                        DataOutputStream os;
+                        os = new DataOutputStream(client1.getOutputStream());
+                        os.writeUTF(usuarios + ":Todos:" + this.nombre + " se desconecto");
+                    }
+                } catch (IOException ex1) {
+                    Logger.getLogger(ChatData.class.getName()).log(Level.SEVERE, null, ex1);
+                }
+                break;
+            }
+        }
+
+    }
+
+}
---
+++ ServerChat/src/serverchat/ServerChat.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverchat;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Aula_info
+ */
+public class ServerChat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        System.out.println("Servidor Listo\n");
+
+        ServerSocket server = new ServerSocket(5000);
+        LinkedList<ChatData> chats = new LinkedList<>();
+        LinkedList<Socket> clients = new LinkedList<>();
+        LinkedList<String> usuarios = new LinkedList<>();
+
+        while (true) {
+            Socket clientsocket = server.accept();
+            DataInputStream entrada = new DataInputStream(clientsocket.getInputStream());
+            ChatData chat1 = new ChatData(clientsocket, entrada.readUTF());
+            chats.add(chat1);
+            clients.add(clientsocket);
+            usuarios.add(chat1.nombre);
+
+            for (ChatData chat : chats) {
+                chat.clients = clients;
+                chat.usuarios = usuarios;
+            }
+
+            DataOutputStream os;
+            for (Socket clien1 : clients) {
+                System.out.println(usuarios + ":Todos:" + chat1.nombre + " conectado");
+                os = new DataOutputStream(clien1.getOutputStream());
+                os.writeUTF(usuarios + ":Todos:" + chat1.nombre + " conectado");
+            }
+
+            chat1.start();
+        }
+
+    }
+
+}
